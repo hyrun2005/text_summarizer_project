eb06da9ccc0f045fcc82afba12bc9ce0d01e44cc
Yurii Hyrun <hyrun2005@gmail.com>
2025-04-06 19:29:33 +0200

    Add data validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from text_summarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from text_summarizer.pipeline.stage02_data_validation import DataValidationPipeline
 from text_summarizer.loggin import logger
 
 STAGE_NAME = 'Data Ingestion stage'
@@ -7,6 +8,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} <<<<<<<<<\n\nx=========")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Data Validation stage'
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+    data_validation = DataValidationPipeline()
+    data_validation.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} <<<<<<<<<\n\nx=========")
 except Exception as e:
     logger.exception(e)
---
+++ src/text_summarizer/pipeline/stage02_data_validation.py
@@ -0,0 +1,16 @@
+from text_summarizer.components.data_validation import DataValidation
+from text_summarizer.config.configuration import ConfigurationManager
+from text_summarizer.loggin import logger
+
+class DataValidationPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            data_validation_config = config.get_data_validation_config()
+            data_validation = DataValidation(config=data_validation_config)
+            data_validation.validate_all_files_exist()
+        except Exception as e:
+            raise e
